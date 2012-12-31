46311a28361d3ddb3b9f81adc2c2bd6ad4c84ee5
pinepain <pinepain@gmail.com>
2013-01-01 01:44:29 +0200

    add composer and try to test php 5.5

--- tests/bootstrap.php
+++ tests/bootstrap.php
@@ -3,3 +3,10 @@
  * @author Ben Pinepain <pinepain@gmail.com>
  * @created 12/31/12 @ 7:09 PM
  */
+
+$file = __DIR__.'/../vendor/autoload.php';
+if (!file_exists($file)) {
+    throw new RuntimeException('Install dependencies to run test suite.');
+}
+
+$autoload = require_once $file;
