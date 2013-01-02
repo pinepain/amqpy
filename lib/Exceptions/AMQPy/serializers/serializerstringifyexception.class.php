188f65089a2a58d815e14f291f5ecc9e6f268353
pinepain <pinepain@gmail.com>
2013-01-02 23:42:43 +0200

    serializers

---
+++ lib/Exceptions/AMQPy/serializers/serializerstringifyexception.class.php
@@ -0,0 +1,11 @@
+<?php
+/**
+ * @author Ben Pinepain <pinepain@gmail.com>
+ * @created 12/4/12 12:46 PM
+ */
+
+namespace Exceptions\Core\Serializers;
+
+
+class SerializerStringifyException extends SerializerException {
+}
