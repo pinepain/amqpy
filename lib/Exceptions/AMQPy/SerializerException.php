88a5ced7ff423e2acef7c22c11e7df67f7e13892
pinepain <pinepain@gmail.com>
2013-01-03 00:03:36 +0200

    fix layout

---
+++ lib/Exceptions/AMQPy/SerializerException.php
@@ -0,0 +1,12 @@
+<?php
+/**
+ * @author Ben Pinepain <pinepain@gmail.com>
+ * @created 12/4/12 12:46 PM
+ */
+
+namespace Exceptions\AMQPy;
+
+use \Exception;
+
+class SerializerException extends Exception {
+}
